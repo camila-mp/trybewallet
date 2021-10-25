fae25e6514a114991ab0f003fb47e5c01b557fb7
camila-mp <ca.malvp@gmail.com>
2021-10-25 13:45:10 -0300

    muda rotas denovo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
@@ -7,10 +7,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="app-div">
-        <Switch>
-          <Route exact path="/trybewallet" component={ Login } />
-          <Route path="/trybewallet/carteira" component={ Wallet } />
-        </Switch>
+        <h2>Oi</h2>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
       </div>
     );
   }
