387079dde8ba5e1464a4692c5747cf09dafa925d
camila-mp <ca.malvp@gmail.com>
2021-10-25 10:46:03 -0300

    Updates

---
+++ precache-manifest.5b04c37d05ee9a997bd1b24d75ff980d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba1689fe79c4d2d2ebc8d8d2fb2e2df",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "709002d03a4ec1c263cd",
+    "url": "/trybewallet/static/css/main.d43aa889.chunk.css"
+  },
+  {
+    "revision": "51e3b8e168f7b337e6be",
+    "url": "/trybewallet/static/js/2.170307e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.170307e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709002d03a4ec1c263cd",
+    "url": "/trybewallet/static/js/main.e13c3918.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
