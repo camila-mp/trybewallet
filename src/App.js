3fce51729e2feb5ec1fc7631f6eaa23208f449d8
camila-mp <ca.malvp@gmail.com>
2021-10-25 14:00:19 -0300

    remove o h2 de App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="app-div">
-        <h2>Oi</h2>
         <Route exact path="/" component={ Login } />
         <Route path="/carteira" component={ Wallet } />
       </div>
